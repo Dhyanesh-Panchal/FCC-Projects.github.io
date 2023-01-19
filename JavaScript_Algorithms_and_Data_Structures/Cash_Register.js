1af668376b6ac1138e63c39c360ed484b1c4c7ca
Dhyanesh Panchal <dhynincoding2004@gmail.com>
2023-01-19 23:57:40 +0530

    Today's FCC practice

--- JavaScript_Algorithms_and_Data_Structures/Cash_Register.js
+++ JavaScript_Algorithms_and_Data_Structures/Cash_Register.js
@@ -1,11 +1,12 @@
 function checkCashRegister(price, cash, cid) {
-    let change;
+    let change = [];
+    let changeAmount = cash - price;
     let money = {
         "PENNY": 0.01,
         "NICKEL": 0.05,
         "DIME": 0.1,
         "QUARTER": 0.25,
-        "DOLLAR": 1,
+        "ONE": 1,
         "FIVE": 5,
         "TEN": 10,
         "TWENTY": 20,
@@ -17,8 +18,50 @@ function checkCashRegister(price, cash, cid) {
         totalcid += cid[i][1];
     }
     totalcid = (totalcid * 100) / 100; //rounding it to 2 decimal
-    console.log(totalcid);
-    return change;
+    // console.log(totalcid);
+    if (totalcid < changeAmount) {
+        console.log({ status: "INSUFFICIENT_FUNDS", change: [] })
+        return { status: "INSUFFICIENT_FUNDS", change: [] }
+    }
+    else if (totalcid == changeAmount) {
+        console.log({ status: "CLOSED", change: cid })
+        return { status: "CLOSED", change: cid }
+
+    }
+    cid = cid.reverse();
+    // console.log(cid);
+
+    for (let val of cid) {
+        if (changeAmount >= val[1]) {
+            // console.log(val);
+            // console.log("inside 1st if: ", changeAmount, val[1])
+            changeAmount -= val[1];
+            change.push(val);
+        } else {
+            let unitChange = 0;
+            while (changeAmount >= money[val[0]]) {
+                console.log("inside while", changeAmount, money[val[0]])
+                changeAmount -= money[val[0]];
+                unitChange += money[val[0]];
+                // console.log(money[val[0]], changeAmount, unitChange, change);
+                changeAmount = (Math.round(changeAmount * 100)) / 100;
+            }
+            if (unitChange) {
+                change.push([val[0], unitChange])
+                // console.log("pusshing the value", change, changeAmount);
+            }
+        }
+        if (changeAmount <= 0) {
+            // console.log("Success!!!!!!!!", { status: "OPEN", change: change })
+            // console.log("Before return")
+            return { status: "OPEN", change: change };
+        }
+        console.log(changeAmount, change)
+    };
+    console.log("The last one:", { status: "INSUFFICIENT_FUNDS", change: [] })
+    return { status: "INSUFFICIENT_FUNDS", change: [] };
 }
 
-checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
+
+// -> {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}
