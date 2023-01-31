35e3fe9a26f354e4a5adff9ce022776ab75351a6
Dhyanesh Panchal <dhynincoding2004@gmail.com>
2023-01-31 22:29:50 +0530

    Added basic perview features (# ,## ,### ,-  ) with inline nesting

--- Frontend Development Libraries/markdown-previewer/src/Preview.jsx
+++ Frontend Development Libraries/markdown-previewer/src/Preview.jsx
@@ -1,23 +1,51 @@
 import React from 'react'
 
+let LineParcer = (props) => {
 
+    return (
+        props.Lines.map((key) => {
+            console.log('The key is:', key)
+
+            if (key.slice(0, 2) === '# ') {
+                console.log("h1")
+                key = key.slice(2);
+                return <h1><LineParcer Lines={[key]} /></h1>
+            }
+            else if (key.slice(0, 3) === '## ') {
+                console.log("h2")
+                key = key.slice(3);
+                return <h2><LineParcer Lines={[key]} /></h2>
+            }
+            else if (key.slice(0, 4) === '### ') {
+                console.log("h3")
+                key = key.slice(4);
+                return <h3><LineParcer Lines={[key]} /></h3>
+            }
+            else if (key.slice(0, 2) === '- ') {
+                console.log("list")
+                key = key.slice(2);
+                console.log(key)
+                return (<ul><li><LineParcer Lines={[key]} /></li></ul>)
+            }
+            else {
+                console.log("last else, and key is:", key);
+                return <p>{key}</p>
+            }
+
+        }
+        )
+    )
+}
 
 const Preview = (props) => {
     // console.log(typeof props.text)
-    let myTexts = props.text.split(/\n/);
-    console.log(myTexts);
+    let Lines = props.text.split(/\n/);
+    // console.log(Lines);
     return (
         <div>
             <h2 style={{ display: 'block' }}>Preview</h2>
             <div className='preview-box'>
-                <ul>
-                    {
-                        myTexts.map((key, indx) => {
-                            if (key)
-                                return (<li id={indx}>{key}</li>)
-                        })
-                    }
-                </ul>
+                <LineParcer Lines={Lines} />
             </div>
         </div>
 
