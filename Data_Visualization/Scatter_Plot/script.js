63ccae8aee4953d9bb0b9b8a7093120765cf4af0
Dhyanesh Panchal <dhynincoding2004@gmail.com>
2023-02-17 00:39:49 +0530

    done with x-Axis, y-Axis is under construction

--- Data_Visualization/Scatter_Plot/script.js
+++ Data_Visualization/Scatter_Plot/script.js
@@ -7,13 +7,13 @@ const Plotdata = (data) => {
     const svg = d3.select('.graph').append('svg').style('width', svgDim.w).style('height', svgDim.h);
 
 
-    let xMax = d3.max(data, d => d.Year);
-    let xMin = d3.min(data, d => d.Year);
-    let yMax = d3.max(data, d => d.Time);
-    let yMin = d3.min(data, d => d.Time);
+    let xMax = d3.max(data, d => d.formatedTime);
+    let xMin = d3.min(data, d => d.formatedTime);
+    let yMax = d3.max(data, d => d.formatedTime);
+    let yMin = d3.min(data, d => d.formatedTime);
     // console.log(xMax)
 
-    const xScale = d3.scaleLinear()
+    const xScale = d3.scaleTime()
         .domain([xMin, xMax])
         .range([svgDim.padding, svgDim.w - svgDim.padding]);
 
@@ -24,9 +24,24 @@ const Plotdata = (data) => {
     xAxis = d3.axisBottom(xScale);
     svg.append('g').attr('transform', `translate(0,${svgDim.h - svgDim.padding})`).call(xAxis).attr('id', 'x-axis');
 
-
+    let timeFormat = d3.timeFormat("%M:%S");
     yAxis = d3.axisLeft(yScale);
-    svg.append('g').attr('transform', `translate(${svgDim.padding},0)`).call(yAxis).attr('id', 'y-axis');
+    svg.append('g')
+        .attr('transform', `translate(${svgDim.padding},0)`)
+        .call(yAxis)
+        .attr('id', 'y-axis')
+        ;
+
+
+    //Plotting circles
+    svg.selectAll('circle')
+        .data(data)
+        .enter()
+        .append('circle')
+        .attr('class', 'dot')
+        .attr('r', 5)
+        .attr('data-xvalue', d => d.Year)
+        .attr('data-yvalue', d => d.formatedTime)
 
     console.log('recieved data is : ', data);
 
@@ -36,6 +51,13 @@ const getData = async (URL) => {
     const res = await fetch(URL);
     const jsonData = await res.json();
     let dataset = await jsonData;
+
+    dataset = dataset.map((key) => {
+        // console.log(key.Time.slice(0, 2), key.Time.slice(3, 5))
+        let time = new Date(0, 0, 0, 0, 0, 0, key.Seconds * 1000); // passing mili-seconds
+        console.log(time)
+        return { ...key, formatedTime: time }
+    })
 
     Plotdata(dataset);
 }
