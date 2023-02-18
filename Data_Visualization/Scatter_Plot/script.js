2f8e0b4f125e32e621d59470e4b75ef87724831d
Dhyanesh Panchal <dhynincoding2004@gmail.com>
2023-02-18 22:24:47 +0530

    y-Scale ticks alignment problem solved

--- Data_Visualization/Scatter_Plot/script.js
+++ Data_Visualization/Scatter_Plot/script.js
@@ -11,20 +11,21 @@ const Plotdata = (data) => {
     let xMin = d3.min(data, d => d.Year);
     let yMax = d3.max(data, d => d.formatedTime);
     let yMin = d3.min(data, d => d.formatedTime);
-    console.log(yMin)
+    // console.log(yMin)
+    // console.log(yMax)
 
     const xScale = d3.scaleLinear()
-        .domain([xMin, xMax])
+        .domain([xMin - 1, xMax + 1])
         .range([svgDim.padding, svgDim.w - svgDim.padding]);
 
     const yScale = d3.scaleTime()
         .domain([yMin, yMax])
-        .range([svgDim.h - svgDim.padding, svgDim.padding]);
+        .range([svgDim.padding, svgDim.h - svgDim.padding]);
 
     xAxis = d3.axisBottom(xScale).tickFormat(d3.format('d'));// used to cinvert , seperated arguments into decimal
     svg.append('g').attr('transform', `translate(0,${svgDim.h - svgDim.padding})`).call(xAxis).attr('id', 'x-axis');
 
-    let timeFormat = d3.timeFormat("%M:%S");
+    // let timeFormat = d3.timeFormat("%M:%S");
     yAxis = d3.axisLeft(yScale).tickFormat(d3.timeFormat('%M:%S'));
     svg.append('g')
         .attr('transform', `translate(${svgDim.padding},0)`)
@@ -39,10 +40,11 @@ const Plotdata = (data) => {
         .append('circle')
         .attr('class', 'dot')
         .attr('cx', d => xScale(d.Year))
-        .attr('cy', d => svgDim.h - yScale(d.formatedTime))
+        .attr('cy', d => yScale(d.formatedTime))
         .attr('r', 5)
         .attr('data-xvalue', d => d.Year)
         .attr('data-yvalue', d => d.formatedTime)
+        .attr('fill', d => { if (d.Doping) { return 'rgb(31, 119, 180)' } else { return 'rgb(255, 127, 14)' } })
 
     console.log('recieved data is : ', data);
 
@@ -55,7 +57,7 @@ const getData = async (URL) => {
 
     dataset = dataset.map((key) => {
         // console.log(key.Time.slice(0, 2), key.Time.slice(3, 5))
-        let time = new Date(0, 0, 0, 0, 0, 0, key.Seconds * 1000); // passing mili-seconds
+        let time = new Date(2000, 1, 1, 1, ...key.Time.split(':')); // passing minutes and seconds
         // console.log(time)
         return { ...key, formatedTime: time }
     })
