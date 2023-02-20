092dd9dcb2855c1f2348408e8eaf7b03a5dcdc3c
Dhyanesh Panchal <dhynincoding2004@gmail.com>
2023-02-21 01:07:04 +0530

    Axis is perfect,bug in rect-fill-color and rect- dimentioning remaining

--- Data_Visualization/Heat_Map/script.js
+++ Data_Visualization/Heat_Map/script.js
@@ -1,32 +1,86 @@
-const Plotdata = (data) => {
-    const svgDim = {
-        w: 1000,
-        h: 600,
-        padding: 50
+const getColor = (val) => {
+    if (val < -5.5) {
+        return 'rgb(69, 117, 180)'
     }
-    const svg = d3.select('.graph').append('svg').style('width', svgDim.w).style('height', svgDim.h);
+    else if (val < -4) {
+        return 'rgb(116, 173, 209)'
+    }
+    else if (val < -2.5) {
+        return 'rgb(171, 217, 233)'
+    }
+    else if (val < -1) {
+        return 'rgb(224, 243, 248)'
+    }
+    else if (val < 1) {
+        return 'rgb(255, 255, 191)'
+    }
+    else if (val < 2) {
+        return 'rgb(254, 224, 144)'
+    }
+    else if (val < 3) {
+        return 'rgb(253, 174, 97)'
+    }
+    else if (val < 4.5) {
+        return 'rgb(244, 109, 67)'
+    }
+    else {
+        return 'rgb(215, 48, 39)'
+    }
+}
+
+const Plotdata = (data) => {
+    const baseTemp = data.baseTemperature, monthlyData = data.monthlyVariance;
+    console.log(monthlyData)
+    const svgDim = {
+        w: 1400,
+        h: 600,
+        padding: 70
+    }
+    const svg = d3.select('.graph')
+        .append('svg')
+        .style('width', svgDim.w)
+        .style('height', svgDim.h);
 
 
-    let xMax = d3.max(data, d => d[0]);
-    let xMin = d3.min(data, d => d[0]);
-    let yMax = d3.max(data, d => d[1]);
-    let yMin = d3.min(data, d => d[1]);
-    // console.log(xMax)
+    let xMax = d3.max(monthlyData, d => d.year);
+    let xMin = d3.min(monthlyData, d => d.year);
+    console.log(d3.max(monthlyData, d => d.variance))
+    // console.log(yMax)
 
-    const xScale = d3.scaleTime()
+    const xScale = d3.scaleLinear()
         .domain([xMin, xMax])
         .range([svgDim.padding, svgDim.w - svgDim.padding]);
 
-    const yScale = d3.scaleLinear()
-        .domain([0, yMax])
-        .range([svgDim.h - svgDim.padding, svgDim.padding]);
+    const yScale = d3.scaleTime()
+        .domain([new Date(2000, 0), new Date(2000, 11)])
+        .range([svgDim.padding, svgDim.h - svgDim.padding]);
 
-    xAxis = d3.axisBottom(xScale);
+    xAxis = d3.axisBottom(xScale).tickFormat(d3.format('d')).ticks([26]);// used to cinvert , seperated arguments into decimal
     svg.append('g').attr('transform', `translate(0,${svgDim.h - svgDim.padding})`).call(xAxis).attr('id', 'x-axis');
 
+    // let timeFormat = d3.timeFormat("%M:%S");
+    yAxis = d3.axisLeft(yScale).tickFormat(d3.timeFormat('%B'));
+    svg.append('g')
+        .attr('transform', `translate(${svgDim.padding},0)`)
+        .call(yAxis)
+        .attr('id', 'y-axis');
 
-    yAxis = d3.axisLeft(yScale);
-    svg.append('g').attr('transform', `translate(${svgDim.padding},0)`).call(yAxis).attr('id', 'y-axis');
+
+    let timeFormat = d3.timeFormat('%m')
+
+    svg.selectAll('rect')
+        .data(monthlyData)
+        .enter()
+        .append('rect')
+        .attr('class', 'cell')
+        .attr('fill', d => getColor(d.variance))
+        .attr('data-month', d => d.month - 1)
+        .attr('data-year', d => d.year)
+        .attr('data-temp', d => d.variance + baseTemp)
+        .attr('x', d => xScale(d.year))
+        .attr('y', d => yScale(new Date(2000, timeFormat(d.Date) - 1)))
+        .attr('width', 10)
+        .attr('height', 50)
 
 }
 
@@ -42,7 +96,7 @@ const getData = async (URL) => {
             Date: newDate
         }
     })
-    // Plotdata(dataset);
+    Plotdata(dataset);
 }
 
 getData('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json')
