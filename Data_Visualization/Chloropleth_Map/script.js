2cf398e8b7b71bdaf2fcc90393245908b02b576d
Dhyanesh Panchal <dhynincoding2004@gmail.com>
2023-02-24 23:41:22 +0530

    map is visible....!!!!!!!!

--- Data_Visualization/Chloropleth_Map/script.js
+++ Data_Visualization/Chloropleth_Map/script.js
@@ -1,15 +1,39 @@
+/**************************************************************************************************************
+ * New Terms to explore: 
+ *  1) TopoJSON & its features (it is extension of geoJSON)
+ *  2) GeoJSON
+ *  3) path in svg
+ *  4) d3.geoPath()
+ **************************************************************************************************************
+ */
+
+
 const EducationURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json';
 const CountyURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json';
 
+
+
 const Plotdata = (Edata, Cdata) => {
+
+    // ==> Note: id in countyData and fips in educationData are mapped with each other. 
+
+    // geoJSON is has objects with type="feature" and here we have array of multiples feature objects
+    Cdata = Cdata.features; // extracting feature from object
+    console.log(Edata);
+    console.log(Cdata);
     const svgDim = {
         w: 1000,
         h: 600,
         padding: 50
     }
     const svg = d3.select('.graph').append('svg').style('width', svgDim.w).style('height', svgDim.h);
-    console.log(Edata)
-    console.log(Cdata)
+
+    svg.selectAll('path') // path is a svg element which is used to draw paths...:)
+        .data(Cdata)
+        .enter()
+        .append('path')
+        .attr('d', d3.geoPath())
+
 
 }
 
@@ -19,7 +43,13 @@ const getData = async (EducationURL, CountyURL) => {
 
 
     let CountyURLResponce = await fetch(CountyURL);
-    let CountyData = await CountyURLResponce.json()
+    let CountyData = await CountyURLResponce.json();    // Here the json file obtained is in topojson format 
+
+    // Actual Data recieved here is in topoJSON format in which geometry of the USA map is Created using Polygons and its coordinates are provided here.
+
+    CountyData = topojson.feature(CountyData, CountyData.objects.counties) // this is to convert topojson into geojson(which is understood by d3)
+
+
     Plotdata(EducationData, CountyData);
 }
 
